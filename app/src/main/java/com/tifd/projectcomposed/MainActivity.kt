4260c0d1ee1a155befac250215ab38aa5dc5d22c
bona1507 <bnvt15@student.ub.ac.id>
2024-09-10 08:16:50 +0700

    update text field

--- app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
+++ app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tifd.projectcomposed.ui.theme.ProjectComposeDTheme
@@ -55,11 +65,25 @@ fun MyScreen() {
     ) {
         Text(text = text)
         Spacer(modifier = Modifier.height(16.dp))
-        TextField(
-            value = inputText,
-            onValueChange = { inputText = it },
-            label = { Text("Enter text") }
-        )
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Filled.AccountBox,
+                contentDescription = "Icon Profile",
+                tint = Color.Black,
+                modifier = Modifier.size(25.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            OutlinedTextField(
+                value = inputText,
+                onValueChange = { inputText = it},
+                label = { Text("Masukkan nama")},
+                modifier = Modifier.weight(1f).fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
+            )
+        }
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
             text = inputText
